351678c774304a5fc06274626174221ca0c4c0b8
Mohtesham-azam <78083475+Mohtesham-azam@users.noreply.github.com>
2022-06-13 17:16:36 +0530

    first commit
    file uploaded

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$('.check').click(function() {
+let name = $(this).attr("name")
+if($(this).prop('checked')){
+$('.check[name="'+ name +'"]').prop('required', false)
+} else {
+$('.check[name="'+ name +'"]').prop('required', true)
+}
+$('.check[name="'+ name +'"]').not(this).prop('checked', false)
+}); 
+$("main").on('submit', function(e){
+alert('submited')
+e.preventDefault()
+return false
+})
+});
+function previewImage() {
+var thisElement = event.target
+var file = thisElement.files;
+var fileReader = new FileReader();
+fileReader.onload = function(event) {
+thisElement.nextElementSibling.setAttribute("src", event.target.result);
+}
+thisElement.style.display = 'none';
+fileReader.readAsDataURL(file[0])
+};
